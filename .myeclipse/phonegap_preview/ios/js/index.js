6a0cc5f0bc10648e628ee80d597052cb317fb68b
shaun088 <Shauncarey@Shauns-MacBook-Pro.local>
2016-05-02 14:40:08 +1000

    geo p-in
    geo p-in

--- .myeclipse/phonegap_preview/ios/js/index.js
+++ .myeclipse/phonegap_preview/ios/js/index.js
@@ -47,4 +47,32 @@ var app = {
 };
 
 app.initialize();
-    
+    function geo()
+    {
+
+        // on
+        // onSuccess Callback
+        // This method accepts a Position object, which contains the
+        // current GPS coordinates
+        //
+        var onSuccess = function(position) {
+            alert('Latitude: '          + position.coords.latitude          + '\n' +
+                  'Longitude: '         + position.coords.longitude         + '\n' +
+                  'Altitude: '          + position.coords.altitude          + '\n' +
+                  'Accuracy: '          + position.coords.accuracy          + '\n' +
+                  'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+                  'Heading: '           + position.coords.heading           + '\n' +
+                  'Speed: '             + position.coords.speed             + '\n' +
+                  'Timestamp: '         + position.timestamp                + '\n');
+        };
+        
+        //Error Callback receives a PositionError object
+        //
+        function onError(error) {
+            alert('code: '    + error.code    + '\n' +
+                  'message: ' + error.message + '\n');
+        }
+
+        navigator.geolocation.getCurrentPosition(onSuccess, onError);
+
+    }
